b89cf2837c5931a575e21fad130dae5ae9379db0
shankaragoudab <140387294+shankaragoudab@users.noreply.github.com>
2025-10-07 16:00:31 +0530

    KB-11681 : fix for logo appears after refreshing the page

--- ansible/artifacts/sunbird/login/resources/js/login.js
+++ ansible/artifacts/sunbird/login/resources/js/login.js
@@ -8,7 +8,7 @@ window.onload = function () {
 	var isForgetPasswordAllow = getValueFromSession('version');
 	var renderingType = 'queryParams';
 	if (window.location.href.indexOf('iiidem-portal') !== -1) {
-		sessionStorage.setItem('rootTenantLogo', "IIIDEM");
+		document.getElementsByClassName('logo-image')[0].src = "/auth/resources/7.0.1/login/sunbird/img/iiidem.png";
 	} else {
 		sessionStorage.setItem('rootTenantLogo', "");
 	}
